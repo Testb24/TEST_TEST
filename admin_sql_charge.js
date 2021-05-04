52e93ba3dd76d4560cea5d18d1badbbebae1b1dd
e.buckel@orange.fr <e.buckel@orange.fr>
2021-05-04 15:07:12 +0200

    aaa

--- admin_sql_charge.js
+++ admin_sql_charge.js
@@ -1,6 +1,8 @@
 // const url = "http://localhost:3000";
 // const url = "https://test-trav.herokuapp.com";
 // const url = "https://trav-server-0-2.herokuapp.com";
+let url = 'http://localhost:3000';
+
 
 const DATA_TEST_TG =
     `INSERT INTO \`x_world\` VALUES (62,-139,200,1,19596,'AZERAZER',2388,'dada49',30,'ALONE',222,null); 
@@ -19,61 +21,89 @@ INSERT INTO \`x_world\` VALUES (4045,-166,190,3,19236,'1',761,'Neywok',0,'',180,
 const btn_s2 = document.getElementById("sql_charge_all_s2");
 const btn_s3 = document.getElementById("sql_charge_all_s3");
 
-btn_s2.addEventListener("click", function () { dl_map_SQL(4, "ts2.travian.fr") });
-btn_s3.addEventListener("click", function () { dl_map_SQL(4, "ts3.travian.fr") });
+btn_s2.addEventListener("click", function () { dl_map_SQL("ts2.travian.fr", url) });
+btn_s3.addEventListener("click", function () { dl_map_SQL("ts3.travian.fr", url) });
 
-async function dl_map_SQL(option, server) {
-    // let server = server;
-    // let option = option;
 
-    console.log(option);
+//=============================================================
+//Fonction principale
+//=============================================================
+async function dl_map_SQL(server, url) {
 
-    //Récupère les _id & pop déjà enregistrés de chaque village :
-    let olddata_village = await getData_old_SQL("town", server);
+    //Récupère les _id & pop déjà enregistrés de chaque town :
+    let olddata_village = await getData_old_SQL("town", server, url);
 
-    // console.log(olddata_village)
+    async function getData_old_SQL(type, server, url) {
+        console.log(server)
+        let url_SQL;
+        url_SQL = url + "/api/server/" + server + "/" + type;
 
+        const response = await fetch(url_SQL, {
+            method: 'GET',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+        });
+
+        return response.json();
+    };
+
+    //Construit l'array des id de town
     let array_village = [];
     olddata_village.forEach(element => {
         array_village.push(element._id);
     });
 
-    // console.log(array_village);
+    let sql_cleaned = await clean_sql_de_travian(server);
 
-    // let test_array = olddata_village.map(element => element._id)
-    // console.log(test_array)
+    //DATA TEST CHOICE
+    async function clean_sql_de_travian(server) {
+        // let DATA_FROM_TG = await getData_new_SQL(server);
+        let DATA_FROM_TG = DATA_TEST_TG;
 
-    testYY = await clean_sql_de_travian(server);
+        DATA_FROM_TG = ");" + DATA_FROM_TG.replace(/\r|\n/g, '').replace(/\); INSERT INTO `x_world` VALUES \(/g, '\);INSERT INTO `x_world` VALUES \(');
 
+        let DATA_FROM_TG_ARRAY = DATA_FROM_TG.split(');INSERT INTO `x_world` VALUES (');
+        DATA_FROM_TG_ARRAY[0] = DATA_FROM_TG_ARRAY[0].substring(34);
+
+        let aaa = DATA_FROM_TG_ARRAY.shift();
+
+        let testYY;
+
+        testYY = DATA_FROM_TG_ARRAY;
+
+        return testYY;
+    };
+
+
+    //Construit la date
     let d = new Date();
     let test_date = d.getDate() + "/" + (d.getMonth() + 1);
-    // test_date = "13/4";
     console.log(test_date);
+
     let array_new_player_id = [];
     let array_new_player = [];
 
     let array_new_ally_id = [];
     let array_new_ally = [];
 
-    testYY.forEach(element => {
-        element = element.split(',');
-        // console.log(element)
+    sql_cleaned.forEach(town_sql => {
+        town_sql = town_sql.split(',');
 
-        if (option == 1 || option == 4) {
-            save_town(olddata_village, array_village, element, test_date, server);
-        }
-
+        let vivi_parsed = save_town(olddata_village, array_village, town_sql, test_date, server);
         function save_town(olddata_village, array_village, element, test_date, server) {
-            // console.log(array_village);
+            let element_vivi;
+
             if (!array_village.includes(parseInt(element[4]))) {
 
-                // console.log("faut enregistrer");
-
                 if (element[5] != "") { element[5] = element[5].substring(1, element[5].length - 1) }
-                // if(element[7]!=""){element[5].substring(1, element[5].length - 1)}
-                // if(element[9]!=""){element[5].substring(1, element[5].length - 1)}
 
-                let element_vivi = {
+                element_vivi = {
                     F: element[0],
                     X: element[1],
                     Y: element[2],
@@ -85,23 +115,15 @@ async function dl_map_SQL(option, server) {
                     Un: element[7].substring(1, element[7].length - 1),
                     Aid: element[8],
                     An: element[9].substring(1, element[9].length - 1),
-                    Pop: [element[10]],
+                    Pop: [parseInt(element[10])],
                     Day: [test_date]
                 };
-                // console.log(element_vivi);
-
-                // console.log(element_vivi);
-                // console.log(element);
                 let url_api;
-                // url_api = "https://test-trav.herokuapp.com//sql/data/town/s2";
-                // url_api = "http://localhost:3000/sql/data/town/s2";
 
-                // url_api = url + "/sql/data/town/s2";
                 url_api = url + "/api/server/" + server + "/sql/town";
 
                 postData_SQL(url_api, element_vivi)
                     .then(data => {
-                        // console.log(data); // JSON data parsed by `data.json()` call
                     });
 
             } else {
@@ -109,10 +131,10 @@ async function dl_map_SQL(option, server) {
                 // console.log("peut-être maj")
                 if (!ccc.Day.includes(test_date)) {
                     // console.log("maj")
-                    let QQ = ccc.Pop.push(element[10]);
+                    let QQ = ccc.Pop.push(parseInt(element[10]));
                     let WW = ccc.Day.push(test_date);
 
-                    let element_vivi = {
+                    element_vivi = {
                         F: element[0],
                         X: element[1],
                         Y: element[2],
@@ -139,56 +161,56 @@ async function dl_map_SQL(option, server) {
                     // console.log("pas maj")
                 }
             }
+            return element_vivi;
         };
 
-        if (option == 2 || option == 4) {
-            build_player(element, array_new_player, array_new_player_id);
-        }
+        // console.log(vivi_parsed);
 
-        function build_player(element, array_new_player, array_new_player_id) {
-
+        build_player(town_sql, array_new_player, array_new_player_id, vivi_parsed);
+        function build_player(element, array_new_player, array_new_player_id, vivi_parsed) {
             let element_player;
 
-            // console.log(array_new_player_id)
+            // console.log(array_new_player_id);
             // console.log(array_new_player)
             // console.log(element[6])
 
-            if (!array_new_player_id.includes(element[6])) {
+            if (!array_new_player_id.includes(server + '_' + element[6])) {
+                // console.log("create new player");
                 element_player = {
                     _id: server + '_' + element[6],
                     Uid: element[6],
                     Un: element[7].substring(1, element[7].length - 1),
                     Aid: element[8],
                     An: element[9].substring(1, element[9].length - 1),
-                    Vivi: [element[4]]
+                    Vivi: [element[4]],
+                    Pop: parseInt(element[10])
                 }
 
-                // console.log("aaa")
+                // console.log("aaa");
                 array_new_player.push(element_player);
-                array_new_player_id.push(element[6]);
+                array_new_player_id.push(server + '_' + element[6]);
             } else {
-                // console.log("add vivi")
+                // console.log("add vivi");
                 let index_of = array_new_player.findIndex(player => player.Uid == parseInt(element[6]));
+
                 // console.log(index_of);
                 // console.log(array_new_player[index_of]);
                 array_new_player[index_of].Vivi.push(element[4]);
+                array_new_player[index_of].Pop += parseInt(element[10]);
             }
-
 
         };
 
-        if (option == 3 || option == 4) {
-            build_ally(element, array_new_ally, array_new_ally_id);
-        }
 
+        build_ally(town_sql, array_new_ally, array_new_ally_id);
         function build_ally(element, array_new_ally, array_new_ally_id) {
 
             if (element[8] == 0) {
                 return
             }
             let element_ally;
-
-            if (!array_new_ally_id.includes(element[8])) {
+            // console.log(array_new_ally_id)
+            if (!array_new_ally_id.includes(server + '_' + element[8])) {
                 // console.log("new ally")
                 element_ally = {
                     _id: server + '_' + element[8],
@@ -197,20 +219,22 @@ async function dl_map_SQL(option, server) {
                     Player: [element[6]]
                 }
                 array_new_ally.push(element_ally);
-                array_new_ally_id.push(element[8]);
-            } else {
+                array_new_ally_id.push(server + '_' + element[8]);
+            } else if (element[8] != "0") {
                 // console.log("add player")
 
-                // console.log();
-                let index_of = array_new_ally.findIndex(ally => ally._id == parseInt(element[8]))
+                // console.log(array_new_ally);
+                // console.log()
+                let index_of = array_new_ally.findIndex(ally => ally._id == server + '_' + element[8])
                 // console.log(index_of);
                 // console.log(array_new_ally[index_of]);
-                if (index_of < 0) {
-                    // console.log(element);
-                    // console.log(index_of);
-                    // console.log(element[8])
-                    return
-                }
+                // if (index_of < 0) {
+                //     // console.log(element);
+                //     // console.log(index_of);
+                //     // console.log(element[8])
+                //     return
+                // }
+                // console.log(array_new_ally[index_of].Player)
                 array_new_ally[index_of].Player.push(element[6])
                 // uniq = [...new Set(array)];
                 array_new_ally[index_of].Player = [... new Set(array_new_ally[index_of].Player)];
@@ -218,48 +242,47 @@ async function dl_map_SQL(option, server) {
             }
         }
 
-
     });
 
-    if (option == 2 || option == 4) {
+    //=========================================
+    //PLAYER PLAYER PLAYER
+    //=========================================
+    console.log(array_new_player_id.length);
+    // console.log(array_new_player_id);
+    console.log(array_new_player);
 
-        // console.log(array_new_player_id);
-        console.log(array_new_player);
-        
-        let url_api;
-        // url_api = "http://localhost:3000/sql/data/player/s2";
-        // url_api = url + "/sql/data/player/s2";
-        url_api = url + "/api/server/" + server + "/sql/player";
+    // url_api = "http://localhost:3000/sql/data/player/s2";
+    // url_api = url + "/sql/data/player/s2";
+    let url_api = url + "/api/server/" + server + "/sql/player";
 
-        array_new_player.forEach(element => {
+    array_new_player.forEach(element => {
 
-            postData_SQL(url_api, element);
+        postData_SQL(url_api, element);
 
-            updateDATA_SQL(url_api, element);
-        });
-    }
+        updateDATA_SQL(url_api, element);
+    });
+    //=========================================
+    //ALLY ALLY ALLY
+    //=========================================
+    console.log(array_new_ally_id.length);
+    // console.log(array_new_ally_id);
+    console.log(array_new_ally);
 
-    if (option == 3 || option == 4) {
+    // url_api = "http://localhost:3000/sql/data/ally/s2";
+    // url_api = url + "/sql/data/ally/s2";
+    url_api = url + "/api/server/" + server + "/sql/ally";
 
-        // console.log(array_new_ally_id);
-        // console.log(array_new_ally);
+    array_new_ally.forEach(element => {
 
-        let url_api;
-        // url_api = "http://localhost:3000/sql/data/ally/s2";
-        // url_api = url + "/sql/data/ally/s2";
-        url_api = url + "/api/server/" + server + "/sql/ally";
+        postData_SQL(url_api, element);
 
-        array_new_ally.forEach(element => {
+        updateDATA_SQL(url_api, element);
+    });
 
-            postData_SQL(url_api, element);
-
-            updateDATA_SQL(url_api, element);
-        });
-    }
-
-    console.log("end")
-
+    console.log("end");
 };
+
+
 //GET 
 async function getData_new_SQL(server) {
     let url_SQL;
@@ -287,27 +310,9 @@ async function getData_new_SQL(server) {
     return response.text();
 };
 
-async function getData_old_SQL(type, server) {
-    let url_SQL;
-    url_SQL = url + "/api/server/" + server + "/" + type;
-
-    const response = await fetch(url_SQL, {
-        method: 'GET',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        redirect: 'follow',
-        referrerPolicy: 'no-referrer',
-    });
-
-    return response.json();
-};
 //POST DATA
 async function postData_SQL(url = '', data = {}) {
-    // Default options are marked with *
+
     const response = await fetch(url, {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors', // no-cors, *cors, same-origin
@@ -326,7 +331,7 @@ async function postData_SQL(url = '', data = {}) {
 };
 // PUT DATA
 async function updateDATA_SQL(url = '', data = {}) {
-    // Default options are marked with *
+
     const response = await fetch(url, {
         method: 'PUT', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors', // no-cors, *cors, same-origin
@@ -344,21 +349,4 @@ async function updateDATA_SQL(url = '', data = {}) {
     return response.json(); // parses JSON response into native JavaScript objects
 };
 
-//DATA TEST CHOICE
-async function clean_sql_de_travian(server) {
-    let DATA_FROM_TG = await getData_new_SQL(server);
-    // let DATA_FROM_TG = DATA_TEST_TG;
 
-    DATA_FROM_TG = ");" + DATA_FROM_TG.replace(/\r|\n/g, '').replace(/\); INSERT INTO `x_world` VALUES \(/g, '\);INSERT INTO `x_world` VALUES \(');
-
-    let DATA_FROM_TG_ARRAY = DATA_FROM_TG.split(');INSERT INTO `x_world` VALUES (');
-    DATA_FROM_TG_ARRAY[0] = DATA_FROM_TG_ARRAY[0].substring(34);
-
-    let aaa = DATA_FROM_TG_ARRAY.shift();
-
-    let testYY;
-
-    testYY = DATA_FROM_TG_ARRAY;
-
-    return testYY;
-};
--- server.js
+++ server.js
@@ -3,35 +3,29 @@ let message_error = document.getElementById('message_error');
 
 
 let DATA_server = Object;
+let params = new URLSearchParams(document.location.search.substring(1));
+let server = params.get("a");
+let type = params.get("b");
+let id = params.get("c");
 
+// console.log(server)
+// console.log(type)
+// console.log(id)
 
 let temp_var = true;
 let url;
-if (!temp_var) {
-    url = "https://trav-server-0-2.herokuapp.com";
-} else {
-    url = 'http://localhost:3000';
-}
-// // console.log(url);
-// under_title.innerText = 'Stats : ' + type;
-// // console.log(type)
-// if (!id == null) {
-//     // console.log("========AAAA+++++++++++")
-//     under_title.innerText += '(' + id + ')';
-// }
+let i = 0;
+
+// url = 'http://localhost:3000';
+url = "https://trav-server-0-2.herokuapp.com";
 //==============================================================================
 //AU START : - affiche la page / change le type + check if possible 
 //==============================================================================
-let server, type, id;
+window.addEventListener('load', main_loop);
 
-at_start()
+async function main_loop() { //url, DATA_server, server, type, id
+    i++;
 
-function at_start() {
-
-    let params = new URLSearchParams(document.location.search.substring(1));
-    server = params.get("a");
-    type = params.get("b");
-    id = params.get("c");
 
     if (id == null && type != 'general' && type != '0') {
         message_error.innerText = "404 : Ally, joueur ou village non trouvé !";
@@ -58,41 +52,32 @@ function at_start() {
     }
 
     under_title.innerText = "Stats : " + type;
-};
 
-console.log("==================================");
-console.log(type);
-console.log("==================================");
-//==============================================================================
-//Charge la data au start de la page (=> plus rapide)
-//==============================================================================
-// document.addEventListener('DOMContentLoaded', charge_data_at_start());
-// build_table(false, server, type);
-charge_data_at_start(server, url, type, id);
-
-async function charge_data_at_start(server, url, type, id) {
-    // console.log(server)
-    // console.log("TEST RETURN");
-    // console.log(DATA_server.allys)
-    if (DATA_server.allys == undefined) {
-        // console.log('recharge les datas');
-        DATA_server.allys = await requete_server(server, "ally", url);
-        // DATA_server.players = await requete_server(server, "player", url);
-        // DATA_server.towns = await requete_server(server, "town", url);
+    await charge_data();
+    async function charge_data() {
+        try {
+            console.log(DATA_server.allys[1]);
+        } catch (e) {
+            console.log('recharge les datas ' + i + ' ème FOIS');
+            DATA_server.allys = await requete_server(server, "ally", url);
+            DATA_server.players = await requete_server(server, "player", url);
+            DATA_server.towns = await requete_server(server, "town", url);
+            // console.log(DATA_server.allys[1]._id)
+        }
     }
 
-    // console.log(DATA_server);
     // console.log(DATA_server.allys);
     // console.log(DATA_server.players);
     // console.log(DATA_server.towns);
-
+    // console.log(type);
+    // console.log(id);
     build_table(DATA_server, type, id);
 };
 
 async function requete_server(server, type, url) {
 
     let url_temp = url + '/api/server/' + server + '/' + type;
-    console.log(url_temp);
+    // console.log(url_temp);
 
     const response = await fetch(url_temp, {
         method: 'GET', // *GET, POST, PUT, DELETE, etc.
@@ -118,49 +103,77 @@ function build_table(DATA_server, type, id) {
 
     let DATA_temp;
     let table_top = document.createElement('tr');
-    // let table_content = document.createElement('tr');
-    let element = document.createElement('tr');
 
-    console.log(type);
-    console.log(id);
-    console.log(server);
-
-    console.log(DATA_server.allys);
+    let DATA_ally, DATA_player, DATA_tow;
 
     switch (type) {
-        case ("ally"): DATA_temp = DATA_server.allys;
+        case ("general"):
+            console.log("GENERAL")
+            break
+        case ("ally"):
+            // DATA_temp = DATA_server.allys;
             table_top.innerHTML =
                 "<th>Joueur</th>" +
                 "<th>Nbr de vivi</th>" +
                 "<th>Pop</th>";
             table.appendChild(table_top);
+            // console.log(DATA_server.players);
+            DATA_ally = DATA_server.allys.find(ally => ally.Aid == id);
+            DATA_player = DATA_server.players.filter(player => player.Aid == id);
 
-            let ally = DATA_server.allys.find(element => element._id == server + '_' + id);
-
-            ally.Player.forEach(player => {
+            under_title.innerText = "Alliance " + DATA_ally.An + ' ( ' + DATA_ally.Player.length + ' joueurs )';
+            // console.log(DATA_ally)
+            // console.log(DATA_ally.An)
+            // console.log(DATA_player)
+            DATA_player.forEach(player => {
+                let element = document.createElement('tr');
+                // console.log('==============')
+                // console.log(window.location.origin + window.location.pathname + '?a=' + server + '&b=2&c=' + player.Uid)
                 element.innerHTML =
-                    "<td>"+player+"<a href=\"https://"+server+"/profile/"+player+"\" target=\"_blank\"> \&#8663</a></td>" +
-                    "<td>" + player + "</td>" +
-                    "<td>" + player + "</td>";
+                    "<td>" +
+                    "<a href=\"" + window.location.origin + window.location.pathname + '?a=' + server + '&b=2&c=' + player.Uid + "\">" + player.Un + "</a>" +
+                    "<a href=\"https://" + server + "/profile/" + player.Uid + "\" target=\"_blank\"> \&#8663</a>" +
+                    "</td>" +
+                    "<td>" + player.Vivi.length + "</td>" +
+                    "<td>" + player.Pop + "</td>";
                 table.appendChild(element);
-            });
+                // console.log(table);
+            })
             break;
+
         case ("player"): DATA_temp = DATA_server.players;
             table_top.innerHTML =
                 "<th>Vivi</th>" +
                 "<th>Pop</th>" +
                 "<th>Position</th>";
-                table.appendChild(table_top);
+            table.appendChild(table_top);
 
-                let player = DATA_server.players.find(element => element._id == server + '_' + id);
+            DATA_player = DATA_server.players.find(player => player.Uid == id);
+            // console.log(DATA_player)
+            under_title.innerText = "Joueur " + DATA_player.Un + ' ( ' + DATA_player.Vivi.length + ' villages )';
+            under_title.innerHTML += "<a href=\"https://" + server + "/profile/" + DATA_player.Uid + "\" target=\"_blank\"> \&#8663</a>";
 
-                player.town.forEach(player => {
-                    element.innerHTML =
-                        "<td>" + player + "</td>" +
-                        "<td>" + player + "</td>" +
-                        "<td>" + player + "</td>";
-                    table.appendChild(element);
-                });
+            DATA_town = DATA_server.towns.filter(town => town.Uid == id);
+            // console.log(DATA_town)
+
+            DATA_town.forEach(town => {
+                // console.log(town)
+                let element = document.createElement('tr');
+                element.innerHTML =
+                    "<td>" +
+                    town.Vn +
+                    "<a href=\"https://" + server + "/profile/" + town.Uid + "\" target=\"_blank\"> \&#8663</a>" +
+                    "</td>" +
+                    "<td>" + town.Pop[town.Pop.length - 1] + "</td>" +
+                    "<td>" +
+                    "<a href=\"https://" + server + "/position_details.php?x=" + town.X + "&y=" + town.Y + "\" target=\"_blank\">" + town.X + "/" + town.Y + "</a>"
+                "</td>";
+                table.appendChild(element);
+            })
+            let message = document.createElement('div');
+            message.innerHTML = "Ally : <a href=\"" + window.location.origin + window.location.pathname + '?a=' + server + '&b=1&c=' + DATA_player.Aid + "\">" + DATA_player.An + "</a>";
+            table.parentElement.appendChild(message);
+            // console.log(table.parentElement);
             break;
         case ("town"): DATA_temp = DATA_server.towns;
             break;
@@ -172,7 +185,7 @@ function build_table(DATA_server, type, id) {
     // }
 
 
-    console.log(table);
+    // console.log(table);
 
     // console.log(DATA_temp);
 
